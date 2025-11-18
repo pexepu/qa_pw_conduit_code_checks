ac72004e3f337619f3b2faa740fbb9f2cbec9a12
pexepu <pexepu@tutanota.com>
2025-11-18 12:01:24 +0000

    test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,31 +3,29 @@ import pluginJs from '@eslint/js';
 import playwright from 'eslint-plugin-playwright';
 import eslintConfigPrettier from 'eslint-config-prettier';
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
- { languageOptions: { globals: globals.node } },
- eslintConfigPrettier,
- {
-   ...pluginJs.configs.recommended,
-   ...playwright.configs['flat/recommended'],
-   rules: {
-     ...pluginJs.configs.recommended.rules,
-     'no-unused-vars': 'error',
-     'max-len': [
-       'error',
-       {
-         code: 80,
-         comments: 80,
-       },
-     ],
-     ...playwright.configs['flat/recommended'].rules,
-     'playwright/expect-expect': 'off',
-   },
-   ignores: [
-     '**/node_modules/*',
-     'playwright.config.js',
-     '**/playwright-report/**',
-   ],
- },
+  {
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+
+  pluginJs.configs.recommended,
+
+  playwright.configs['flat/recommended'],
+
+  {
+    rules: {
+      'no-unused-vars': 'error',
+      'playwright/expect-expect': 'off',
+    },
+    ignores: [
+      '**/node_modules/**',
+      'playwright.config.js',
+      '**/playwright-report/**',
+    ],
+  },
+
+  eslintConfigPrettier,
 ];
