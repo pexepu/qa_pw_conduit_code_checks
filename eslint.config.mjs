c2b7c59d2df069ca9c554162e13d2ce35626200a
pexepu <pexepu@tutanota.com>
2025-11-18 11:46:43 +0000

    resolved

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import playwright from 'eslint-plugin-playwright';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+ { languageOptions: { globals: globals.node } },
+ eslintConfigPrettier,
+ {
+   ...pluginJs.configs.recommended,
+   ...playwright.configs['flat/recommended'],
+   rules: {
+     ...pluginJs.configs.recommended.rules,
+     'no-unused-vars': 'error',
+     'max-len': [
+       'error',
+       {
+         code: 80,
+         comments: 80,
+       },
+     ],
+     ...playwright.configs['flat/recommended'].rules,
+     'playwright/expect-expect': 'off',
+   },
+   ignores: [
+     '**/node_modules/*',
+     'playwright.config.js',
+     '**/playwright-report/**',
+   ],
+ },
+];
